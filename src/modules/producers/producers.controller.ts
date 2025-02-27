3468feefbdc767a83841073675697874de01bd8e
Gustavo <gustrigoni@gmail.com>
2025-02-27 00:44:48 -0300

    add swagger response description

--- src/modules/producers/producers.controller.ts
+++ src/modules/producers/producers.controller.ts
@@ -14,18 +14,21 @@ import {
 import { ProducersService } from './producers.service';
 import { SaveProducerDto } from './dto/saveProducer.dto';
 import { Producer } from '@prisma/client';
-import { ApiParam } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiParam } from '@nestjs/swagger';
 import { ProducerIdParamDto } from './dto/producerIdParam.dto';
 
 @Controller('producers')
 export class ProducersController {
-  private readonly logger = new Logger(ProducersService.name);
+  private readonly logger = new Logger(ProducersController.name);
 
   constructor(private readonly producersService: ProducersService) {}
 
   @Post()
   @Version('1')
   @HttpCode(HttpStatus.CREATED)
+  @ApiCreatedResponse({
+    description: 'Produtor has been created successfully',
+  })
   createProducer(
     @Body() saveCreateProducerDto: SaveProducerDto,
   ): Promise<Producer> {
@@ -46,6 +49,9 @@ export class ProducersController {
   @Patch('/:producerId')
   @Version('1')
   @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    description: 'Produtor has been updated successfully',
+  })
   updateProducer(
     @Body() saveUpdateProducerDto: SaveProducerDto,
     @Param(ValidationPipe) { producerId }: ProducerIdParamDto,
@@ -67,6 +73,9 @@ export class ProducersController {
   @Delete('/:producerId')
   @Version('1')
   @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    description: 'Produtor has been removed successfully',
+  })
   removeProducer(
     @Param(ValidationPipe) { producerId }: ProducerIdParamDto,
   ): Promise<Producer> {
